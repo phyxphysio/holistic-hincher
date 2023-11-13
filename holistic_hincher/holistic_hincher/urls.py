72f9742c665e8cee6c244eabd0b44ed8e3321dea
Liam Miller <admin@phyxphysio.co.nz>
2023-11-13 17:04:36 +1100

    Run collect static and modify allowed hosts

--- holistic_hincher/holistic_hincher/urls.py
+++ holistic_hincher/holistic_hincher/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -80,9 +81,5 @@ urlpatterns = [
     path("memberships/", views.list_memberships, name="memberships"),
 ]
 
-urlpatterns = urlpatterns + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
-urlpatterns = urlpatterns + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
